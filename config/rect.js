8af790b08c8e5338b73fd8579b6bcc7dc22d716d
lyt9304 <513535169@qq.com>
2016-04-28 00:03:29 +0800

    完成数据类和ui类的生成，还需要修改其他地方，优化结构

--- config/rect.js
+++ config/rect.js
@@ -61,42 +61,48 @@ var Rectn = {
 
 		}
 	},
-	"createTemplate": function(){
-		var properties=this.properties;
-		var uuid=this.uuid;
-		var templateStr=
-			'function(x,y,width,height,color,style){\n'+
-			'    var rect=createRect(x,y,width,height,color,style,\''+uuid+'\',0,env.transform);\n'+
-			'    return {\n'+
-			'        '+this.geoOutput[0].varname+':rect,\n'+
-			'        '+this.envOutput[0].varname+':{x:x,y:y,width:width,height:height},\n'+
-			'    };\n'+
-			'}';
-		return templateStr;
+	"method": {
+		"createTemplate": function(){
+			var properties=this.properties;
+			var uuid=this.uuid;
+			var templateStr=
+				'function(x,y,width,height,color,style){\n'+
+				'    var rect=createRect(x,y,width,height,color,style,\''+uuid+'\',0,env.transform);\n'+
+				'    return {\n'+
+				'        '+this.geoOutput[0].varname+':rect,\n'+
+				'        '+this.envOutput[0].varname+':{x:x,y:y,width:width,height:height},\n'+
+				'    };\n'+
+				'}';
+			return templateStr;
+		}
 	},
 	"ui":{
-		"createDom": function(){
-			var that = this;
-			var module = that.module;
-			$(".workflowWindow-sub#" + that.workflowWindow.uuid + ' > .content').append(
-				'<div class="module rectn" id="' + that.uuid + '">' +
-				'<div class="title">' +
-				'<span>' + module.label + '</span>' +
-				'<img src="' + (that.module.layoutIcon||viscomposer.app.imgPool["rectangle"]) + '">' +
-				'</div>' +
-				'<div class="hr"></div>' +
-				'<div class="content">' +
-				'<div class="inputs">' +
-				'</div>' +
-				'<div class="outputs">' +
-				'</div></div></div>');
-		},
-		"update": function(){
-			//RectPanel.baseClass_.prototype.update.call(this);
-			var input = this.module.input;
-			for(var i = 0; i < input.length; i++) {
-				input[i].ui.onSwitch();
-				input[i].ui.update();
+		"productive": false,
+		"pos": [370, 100],
+		"method":{
+			"createDom": function(){
+				var that = this;
+				var module = that.module;
+				$(".workflowWindow-sub#" + that.workflowWindow.uuid + ' > .content').append(
+					'<div class="module rectn" id="' + that.uuid + '">' +
+					'<div class="title">' +
+					'<span>' + module.label + '</span>' +
+					'<img src="' + (that.module.layoutIcon||viscomposer.app.imgPool["rectangle"]) + '">' +
+					'</div>' +
+					'<div class="hr"></div>' +
+					'<div class="content">' +
+					'<div class="inputs">' +
+					'</div>' +
+					'<div class="outputs">' +
+					'</div></div></div>');
+			},
+			"update": function(){
+				//RectPanel.baseClass_.prototype.update.call(this);
+				var input = this.module.input;
+				for(var i = 0; i < input.length; i++) {
+					input[i].ui.onSwitch();
+					input[i].ui.update();
+				}
 			}
 		}
 	}
